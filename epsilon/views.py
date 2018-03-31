3a6767d4ab2462d3937ae7a7429e22a3bec9acc5
Gautam Yadav <gautamyadav@iiitdmj.ac.in>
2018-03-31 11:51:26 +0530

    gvbv

--- epsilon/views.py
+++ epsilon/views.py
@@ -886,7 +886,11 @@ class RBM:
 
 # starts from here
 def recommender_related(rec_for):
-    users = Students.objects.all()  # TODO: Change this to total number of students in the db
+    st = Student.objects.all()  # TODO: Change this to total number of students in the db
+    count = 0
+    for s in st:
+        count = count + 1
+    users = count
     items = 19  # TODO: Change this to total number of courses in the db
     recommend_data = open("/Users/gautam/Desktop/IntelliStudy/grades.csv","w") # TODO: Change this address get grades from scores and store into a csv and avg based on courses
     courses = Course.objects.all()
